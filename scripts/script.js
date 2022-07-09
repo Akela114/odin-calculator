d2959d1b4f4b9a881082737fc694c3588b59def5
Oleg Nikolaev <oleg.nikolaev.nsk@yandex.ru>
2022-07-09 12:53:32 +0700

    Create operate() function

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,20 @@ const multiply = (factorOne, factorTwo) => factorOne * factorTwo;
 
 // Вычислить частное двух чисел
 const divide = (numerator, denominator) => numerator / denominator;
+
+// Выполнить заданную операцию над двумя числами
+const operate = function (operationNum, operandOne, operandTwo) {
+    switch (operationNum) {
+        case 0:
+            return add(operandOne, operandTwo);
+        case 1:
+            return subtract(operandOne, operandTwo);
+        case 2:
+            return multiply(operandOne, operandTwo);
+        case 3:
+            return divide(operandOne, operandTwo);
+        default:
+            console.log('Недопустимое значение параметра operationNum');
+            return null;
+    }
+}
