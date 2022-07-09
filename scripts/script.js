23e5a6b491c2917d9085824bf65a53b37c72c0a5
Oleg Nikolaev <oleg.nikolaev.nsk@yandex.ru>
2022-07-09 12:37:17 +0700

    Create functions of basic arithmetic operations
    Created add(), subtract(), multiply(), divide() functions.

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+// Вычислить сумму двух чисел
+const add = (summandOne, summandTwo) => summandOne + summandTwo;
+
+// Вычислить разность двух чисел
+const subtract = (minuend, subtrahend) => minuend - subtrahend;
+
+// Вычислить произведение двух чисел
+const multiply = (factorOne, factorTwo) => factorOne * factorTwo;
+
+// Вычислить частное двух чисел
+const divide = (numerator, denominator) => numerator / denominator;
