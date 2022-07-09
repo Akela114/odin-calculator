a21e1f6222cfb862d9ee9dc93e4fb73ccc17fc6d
Oleg Nikolaev <oleg.nikolaev.nsk@yandex.ru>
2022-07-09 16:56:56 +0700

    Upgrade calculator logic to work with fractions

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+const PRECISION = 10000;
+
 // Вычислить сумму двух чисел
 const add = (summandOne, summandTwo) => summandOne + summandTwo;
 
@@ -29,16 +31,29 @@ const operate = function (operationType, operandOne, operandTwo) {
 
 // Обновить значение, отображаемое на дисплее
 const updateDisplay = function () {
-    display.textContent = Math.round(operandOne * 100) / 100;
+    display.textContent = Math.round(operandOne * PRECISION) / PRECISION;
+    if (!operationType && isFractionalPart && (operandOne % 1 === 0)) {
+        display.textContent += '.';
+        for (let i = isFractionalPart; i < 0.1; i *= 10) {
+            display.textContent += 0;
+        }
+    }
     if (operationType) {
         display.textContent += operationType;
-        display.textContent += Math.round(operandTwo * 100) / 100;
+        display.textContent += Math.round(operandTwo * PRECISION) / PRECISION;
+        if (isFractionalPart && (operandTwo % 1 === 0)) {
+            display.textContent += '.';
+            for (let i = isFractionalPart; i < 0.1; i *= 10) {
+                display.textContent += 0;
+            }
+        }
     }
 }
 
 let operandOne = 0;
 let operandTwo = 0;
 let operationType = null;
+let isFractionalPart = false;
 
 const display = document.querySelector('.calculator-display');
 updateDisplay();
@@ -47,11 +62,21 @@ const numButtons = document.querySelectorAll('.number');
 numButtons.forEach(button => {
     button.addEventListener('click', event => {
         if (!operationType) {
-            operandOne *= 10;
-            operandOne += +event.target.textContent;
+            if (isFractionalPart) {
+                operandOne += isFractionalPart * +event.target.textContent;
+                isFractionalPart /= 10;
+            } else {
+                operandOne *= 10;
+                operandOne += +event.target.textContent;
+            }
         } else {
-            operandTwo *= 10;
-            operandTwo += +event.target.textContent;
+            if (isFractionalPart) {
+                operandTwo += isFractionalPart * +event.target.textContent;
+                isFractionalPart /= 10;
+            } else {
+                operandTwo *= 10;
+                operandTwo += +event.target.textContent;
+            }
         }
         updateDisplay();
     });
@@ -59,7 +84,10 @@ numButtons.forEach(button => {
 
 // Обновить тип операции
 const updateCalculationsData = opType => {
-    if (!operationType || !operandTwo) operationType = opType;
+    if (!operationType || !operandTwo) {
+        operationType = opType;
+        isFractionalPart = false;
+    }
     else performOperation(opType);
     updateDisplay();
 }
@@ -76,6 +104,7 @@ const performOperation = (opType = null) => {
             operandOne = operate(operationType, operandOne, operandTwo);
             operationType = opType;
             operandTwo = 0;
+            isFractionalPart = false;
             updateDisplay();
         } else {
             alert('Деление на 0 не допускается');
@@ -91,8 +120,17 @@ const resetData = () => {
     operandOne = 0;
     operandTwo = 0;
     operationType = null;
+    isFractionalPart = false;
     updateDisplay();
 }
 
 const clearButton = document.querySelector('.operation-clear');
 clearButton.addEventListener('click', resetData);
+
+const dotButton = document.querySelector('.operation-dot');
+dotButton.addEventListener('click', () => {
+    if (!isFractionalPart) {
+        isFractionalPart = 0.1;
+        updateDisplay();
+    }
+});
